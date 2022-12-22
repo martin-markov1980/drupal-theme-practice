e34638c12be7766fcdb4047f59774799b5df9ae2
mmarkov1980 <martin.markov@weareaccess.co.uk>
2022-12-22 18:39:37 +0000

    Created custom theme.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -778,3 +778,6 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+# Config directory.
+$settings['config_sync_directory'] = $app_root . '/../config/default';
